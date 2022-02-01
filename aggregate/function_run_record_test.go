8528279d7e6b89585da45d00019014615568891b
pillipanda <pillipanda@126.com>
2022-02-01 18:18:01 +0800

    🧪 test(aggregate function_run_record unit test): 100% coverage

---
+++ aggregate/function_run_record_test.go
@@ -0,0 +1,54 @@
+package aggregate
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFunctionRunRecordIsZero(t *testing.T) {
+	Convey("IsZero hit", t, func() {
+		var functionRunRecord *FunctionRunRecord = nil
+		So(functionRunRecord.IsZero(), ShouldBeTrue)
+	})
+
+	Convey("IsZero miss", t, func() {
+		flowRunRecord := NewCrontabTriggeredRunRecord(&fakeFlow)
+		functionRunRecord := NewFunctionRunRecordFromFlowDriven(
+			functionAdd, *flowRunRecord, secondFlowFunctionID)
+		So(functionRunRecord.IsZero(), ShouldBeFalse)
+	})
+
+	Convey("nil attrs check", t, func() {
+		var funcRunRecord *FunctionRunRecord = nil
+		So(funcRunRecord.UsedSeconds(), ShouldEqual, 0)
+		So(funcRunRecord.Failed(), ShouldBeFalse)
+		So(funcRunRecord.Finished(), ShouldBeFalse)
+		So(func() { funcRunRecord.SetSuc() }, ShouldNotPanic)
+		So(func() { funcRunRecord.SetFail("") }, ShouldNotPanic)
+	})
+}
+
+func TestFunctionRunRecord(t *testing.T) {
+	flowRunRecord := NewCrontabTriggeredRunRecord(&fakeFlow)
+	functionRunRecord := NewFunctionRunRecordFromFlowDriven(
+		functionAdd, *flowRunRecord, secondFlowFunctionID)
+
+	Convey("initial FunctionRunRecord attrs check", t, func() {
+		So(functionRunRecord.UsedSeconds(), ShouldBeGreaterThan, 0)
+		So(functionRunRecord.Failed(), ShouldBeFalse)
+		So(functionRunRecord.Finished(), ShouldBeFalse)
+		So(functionRunRecord.Suc, ShouldBeFalse)
+	})
+
+	Convey("set", t, func() {
+		So(functionRunRecord.Suc, ShouldEqual, false)
+		functionRunRecord.SetSuc()
+		So(functionRunRecord.Suc, ShouldBeTrue)
+		So(functionRunRecord.Failed(), ShouldBeFalse)
+		So(functionRunRecord.Finished(), ShouldBeTrue)
+
+		functionRunRecord.SetFail("")
+		So(functionRunRecord.Suc, ShouldEqual, false)
+	})
+}
