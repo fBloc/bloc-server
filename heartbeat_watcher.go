2c5594677fd51b6e14d867e31db7856ea16a83a1
pillipanda <pillipanda@126.com>
2022-03-15 13:03:27 +0800

    🎈 perf(heartbeat_watcher): perf log msg & keep same trace_id

--- heartbeat_watcher.go
+++ heartbeat_watcher.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/fBloc/bloc-server/aggregate"
 	"github.com/fBloc/bloc-server/event"
+	"github.com/fBloc/bloc-server/value_object"
 )
 
 // RePubDeadRuns 重发运行中断的任务
@@ -28,34 +29,43 @@ func (blocApp *BlocApp) RePubDeadRuns() {
 		}
 
 		for _, d := range deads {
-			// 立即进行删除此条信息（利用mongo通过ID删除的原子性保障来确保不会「重复重发」）
-			deleteAmount, err := heartBeatRepo.DeleteByFunctionRunRecordID(d.FunctionRunRecordID)
-			if err != nil {
-				logger.Errorf(
-					map[string]string{},
-					"heartBeatRepo.Delete failed, error: %s", err.Error())
-				continue
-			}
-			if deleteAmount != 1 { // 避免并发watch重复发布
-				continue
+			logTags := map[string]string{
+				string(value_object.SpanID): value_object.NewSpanID(),
+				"business":                  "heartbeat watcher",
+				"function_run_record_id":    d.FunctionRunRecordID.String(),
 			}
 
 			// 查询对应的function run record是否存在
 			funcRunRecord, err := funcRunRecordRepo.GetByID(d.FunctionRunRecordID)
 			if err != nil {
-				logger.Errorf(
-					map[string]string{"function_run_record_id": d.FunctionRunRecordID.String()},
-					"funcRunRecordRepo.GetByID failed. error:: %s", err.Error())
+				logger.Errorf(logTags,
+					"get funcRunRecordRepo by id failed. : %v", err)
 				continue
 			}
 			if funcRunRecord.IsZero() {
+				logger.Warningf(logTags,
+					"get funcRunRecordRepo by id match no record")
 				continue
 			}
+			logTags[string(value_object.TraceID)] = funcRunRecord.TraceID
+			logger.Infof(logTags, "start handle dead heatbeat")
+
 			err = funcRunRecordRepo.ClearProgress(funcRunRecord.ID)
 			if err != nil {
-				logger.Errorf(
-					map[string]string{"function_run_record_id": d.FunctionRunRecordID.String()},
-					"funcRunRecordRepo.ClearProgress: %s", err.Error())
+				logger.Errorf(logTags,
+					"funcRunRecordRepo.ClearProgress failed: %v", err)
+			}
+
+			// 立即进行删除此条信息（利用mongo通过ID删除的原子性保障来确保不会「重复重发」）
+			deleteAmount, err := heartBeatRepo.DeleteByFunctionRunRecordID(d.FunctionRunRecordID)
+			if err != nil {
+				logger.Errorf(logTags,
+					"heartBeatRepo.Delete failed, error: %s", err.Error())
+				continue
+			}
+			if deleteAmount != 1 { // 避免并发watch重复发布
+				logger.Infof(logTags, "already repubed. breakout")
+				continue
 			}
 
 			// 再次进行发布
@@ -63,13 +73,11 @@ func (blocApp *BlocApp) RePubDeadRuns() {
 				FunctionRunRecordID: funcRunRecord.ID,
 			})
 			if err != nil {
-				logger.Errorf(
-					map[string]string{"function_run_record_id": d.FunctionRunRecordID.String()},
-					"pub func event failed. error: %s", err.Error())
+				logger.Errorf(logTags,
+					"pub func event failed: %v", err)
 			} else {
-				logger.Infof(
-					map[string]string{"function_run_record_id": d.FunctionRunRecordID.String()},
-					"re-pub function run record: %s", funcRunRecord.ID)
+				logger.Infof(logTags,
+					"re-pub function run record suc")
 			}
 		}
 	}
